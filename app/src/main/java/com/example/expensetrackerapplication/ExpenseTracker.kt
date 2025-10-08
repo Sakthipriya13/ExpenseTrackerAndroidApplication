db343a7e59e12622adefe45b349f5b8590ccbfcf
SAKTHIPRIYA <sakthipriyaravi2019@gmail.com>
2025-10-08 10:22:18 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/expensetrackerapplication/ExpenseTracker.kt
@@ -0,0 +1,10 @@
+package com.example.expensetrackerapplication
+
+import android.app.Application
+
+class ExpenseTracker : Application()
+{
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
