3511615f4eca40842e89fdd3f5030fd446b74315
SAKTHIPRIYA <sakthipriyaravi2019@gmail.com>
2026-01-08 19:02:44 -0800

    Commit

--- app/src/main/java/com/example/expensetrackerapplication/object/Global.kt
+++ app/src/main/java/com/example/expensetrackerapplication/object/Global.kt
@@ -1,5 +1,13 @@
 package com.example.expensetrackerapplication.`object`
 
+import org.apache.poi.ss.usermodel.BorderStyle
+import org.apache.poi.ss.usermodel.FillPatternType
+import org.apache.poi.ss.usermodel.Font
+import org.apache.poi.ss.usermodel.HorizontalAlignment
+import org.apache.poi.ss.usermodel.IndexedColors
+import org.apache.poi.ss.usermodel.VerticalAlignment
+import org.apache.poi.xssf.usermodel.XSSFCellStyle
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.math.RoundingMode
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -45,4 +53,72 @@ object Global {
     {
         return String.format("%.2f",amt).toFloat()
     }
+
+    fun fnHeaderFont(workBook: XSSFWorkbook) : Font {
+        return workBook.createFont().apply {
+            bold = true
+            fontHeightInPoints = 15
+            color = IndexedColors.BLACK.index
+        }
+    }
+
+    fun fnSummaryFont(workBook: XSSFWorkbook) : Font {
+        return workBook.createFont().apply {
+            bold = true
+            fontHeightInPoints = 13
+            color = IndexedColors.DARK_GREEN.index
+        }
+    }
+
+    fun fnSummaryStyle(workBook: XSSFWorkbook, summaryFont: Font) : XSSFCellStyle {
+        return workBook.createCellStyle().apply {
+            setFont(summaryFont)
+            alignment= HorizontalAlignment.LEFT
+        }
+    }
+
+    fun fnHeaderStyle(workBook: XSSFWorkbook, headerFont: Font) : XSSFCellStyle {
+        return workBook.createCellStyle().apply {
+            setFont(headerFont)
+            alignment= HorizontalAlignment.CENTER
+            verticalAlignment = VerticalAlignment.CENTER
+        }
+    }
+
+    fun fnTableHeaderStyle(workBook: XSSFWorkbook) : XSSFCellStyle {
+        return  workBook.createCellStyle().apply {
+            borderTop = BorderStyle.THICK
+            borderBottom = BorderStyle.THICK
+            borderLeft = BorderStyle.THICK
+            borderRight = BorderStyle.THICK
+
+            setBottomBorderColor(IndexedColors.BLACK.index)
+            setTopBorderColor(IndexedColors.BLACK.index)
+            setLeftBorderColor(IndexedColors.BLACK.index)
+            setRightBorderColor(IndexedColors.BLACK.index)
+
+            alignment = HorizontalAlignment.CENTER
+            fillForegroundColor = IndexedColors.LAVENDER.index
+            fillPattern = FillPatternType.SOLID_FOREGROUND
+            setFont(workBook.createFont().apply { bold = true })
+        }
+    }
+
+
+    fun fnTableDateStyle(workBook: XSSFWorkbook) : XSSFCellStyle {
+        return workBook.createCellStyle().apply {
+            borderTop = BorderStyle.THIN
+            borderBottom = BorderStyle.THIN
+            borderLeft = BorderStyle.THIN
+            borderRight = BorderStyle.THIN
+
+            setBottomBorderColor(IndexedColors.BLACK.index)
+            setTopBorderColor(IndexedColors.BLACK.index)
+            setLeftBorderColor(IndexedColors.BLACK.index)
+            setRightBorderColor(IndexedColors.BLACK.index)
+
+            alignment = HorizontalAlignment.LEFT
+        }
+    }
+
 }
